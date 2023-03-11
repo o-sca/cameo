4415ce6918625f96eaf4bd85856a36279c15c062
daquioag <aldaquioag@gmail.com>
2023-03-11 14:22:41 -0800

    implement button

--- src/contentScript.js
+++ src/contentScript.js
@@ -11,33 +11,34 @@
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
 
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
+(() => {
+  console.log("My content script is running!");
 
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-);
+const button = document.createElement("button");
+button.textContent = "Click me!";
+button.className = "Daquioag";
 
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
+// // Find all elements with the class name "artdeco-entity-lockup__subtitle ember-view"
+let myVar = document.querySelectorAll(".artdeco-entity-lockup__subtitle.ember-view");
+console.log(myVar);
+console.log(myVar.length);
 
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
+const test = document.getElementsByClassName("entity-content");
+console.log(test.length);
+
+if (test.length > 0) {
+  console.log("The element exists");
+} else {
+  console.log("The element does not exist");
+}
+
+const elements = document.querySelectorAll(".entity-content");
+console.log(elements.length);
+
+// Add the button element to each element found
+elements.forEach((element) => {
+  element.appendChild(button.cloneNode(true));
 });
+console.log("My end function again");
+
+})();
