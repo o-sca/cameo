6397a712e274a2cbe06ab80469e7a3708bda4fea
daquioag <aldaquioag@gmail.com>
2023-03-11 17:51:27 -0800

    add button

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,44 +1,37 @@
-'use strict';
+// Create a MutationObserver object
+const observer = new MutationObserver((mutations) => {
+  // Iterate through all the mutations
+  mutations.forEach((mutation) => {
+    // Check if any new nodes were added
+    if (mutation.addedNodes) {
+      // Iterate through all the newly added nodes
+      mutation.addedNodes.forEach((node) => {
+        // Check if the node has the class name "my-class"
+        if (node.className === ("display-flex")) {
+          // Create a button element
+          const button = document.createElement("button");
+          button.textContent = "Click me!";
+          button.className = "my-button";
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+          // Get the text content of the parent div
+          const parentDivText = node.parentElement.textContent;
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
+          // Add a click event listener to the button to log the parent div text
+          button.addEventListener("click", () => {
+            console.log(parentDivText);
+          });
 
-(() => {
-  console.log("My content script is running!");
-
-const button = document.createElement("button");
-button.textContent = "Click me!";
-button.className = "Daquioag";
-
-// // Find all elements with the class name "artdeco-entity-lockup__subtitle ember-view"
-let myVar = document.querySelectorAll(".artdeco-entity-lockup__subtitle.ember-view");
-console.log(myVar);
-console.log(myVar.length);
-
-const test = document.getElementsByClassName("entity-content");
-console.log(test.length);
-
-if (test.length > 0) {
-  console.log("The element exists");
-} else {
-  console.log("The element does not exist");
-}
-
-const elements = document.querySelectorAll(".entity-content");
-console.log(elements.length);
-
-// Add the button element to each element found
-elements.forEach((element) => {
-  element.appendChild(button.cloneNode(true));
+          // Add the button element to the newly added node
+          node.appendChild(button);
+        }
+      });
+    }
+  });
 });
-console.log("My end function again");
 
-})();
+// Start observing the DOM for changes
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
