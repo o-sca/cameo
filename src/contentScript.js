02e87c62fb4496e08af2e6ec7df0c13e799deb03
daquioag <aldaquioag@gmail.com>
2023-03-11 18:43:02 -0800

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,11 +1,15 @@
 // Create a MutationObserver object
+const applicationInformation = {};
+
 const observer = new MutationObserver((mutations) => {
+
   // Iterate through all the mutations
   mutations.forEach((mutation) => {
     // Check if any new nodes were added
     if (mutation.addedNodes) {
       // Iterate through all the newly added nodes
       mutation.addedNodes.forEach((node) => {
+
         // Check if the node has the class name "my-class"
         if (node.className === ("display-flex")) {
           // Create a button element
@@ -15,12 +19,34 @@ const observer = new MutationObserver((mutations) => {
 
 
           // Get the text content of the parent div
-          const parentDivText = node.parentElement.parentElement.textContent;
 
           // Add a click event listener to the button to log the parent div text
           button.addEventListener("click", () => {
-            console.log(parentDivText);
+            const parentDiv = node.parentElement.parentElement;
+
+            if (parentDiv) {
+              const jobTitleElem = parentDiv.querySelector("[class*='job-title']").textContent;
+              const companyName = parentDiv.querySelector("[class*='company-name']").textContent.trim();
+
+              if (jobTitleElem){
+                console.log(jobTitleElem);
+                console.log(companyName);
+                printTodayDate();
+
+              }else{
+                console.log(" no job title")
+
+              }
+              // rest of the code
+            }else{
+              console.log("no parent dive!")
+            }
+            
+
+
           });
+
+
 
           // Add the button element to the newly added node
           node.appendChild(button);
@@ -35,3 +61,9 @@ observer.observe(document.body, {
   childList: true,
   subtree: true
 });
+
+function printTodayDate() {
+  const today = new Date();
+  console.log(today.toDateString());
+}
+
